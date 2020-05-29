a7155a3ebd373a0bd75c30c7a3ce0ac83cd1747b
govind144 <kgovind144@gmail.com>
2020-05-29 13:44:27 +0530

    Added Customer.java,CustomerController.java,customer-form.jsp, customer-confirmation.jsp and update main-menu.jsp file.

---
+++ spring-mvc-demo/src/com/gkn/springmvc/Customer.java
@@ -0,0 +1,27 @@
+package com.gkn.springmvc;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Customer {
+	
+	@NotNull(message="is required")
+	@Size(min=1, message="is required")
+	private String firstName;
+	
+	private String lastName;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+}
